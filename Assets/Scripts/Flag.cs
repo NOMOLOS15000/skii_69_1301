099836a85601d6d7a1c43af31e878bebaa34cf62
NOMOLOS15000 <titaya151014@gmail.com>
2026-08-17 17:28:03 +0700

    Add FlagPre

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,20 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Flag : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+
+        if (player == null)
+            return;
+
+        player.Point += 10;
+
+        UIManager.Instance.ShowNotiText($"+10 points\nPoints: {player.Point}");
+        Destroy(gameObject);
+
+    }
+
+}
